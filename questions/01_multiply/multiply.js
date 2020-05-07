3d1e3b4fe1486c086c491432d6f4f0216aae3f08
JudithBLinder <judithrooz@gmail.com>
2020-05-07 11:54:51 -0400

    multiply solved

--- questions/01_multiply/multiply.js
+++ questions/01_multiply/multiply.js
@@ -1,5 +1,12 @@
 // This stretch requires you to write your own test cases
 
-const multiply = (num1, num2) => {};
+const multiply = (num1, num2) => {
+    if (typeof(num1) != 'number' || typeof(num2) != 'number') {
+        throw 'not a number!';
+    }
+
+    let result = num1 * num2;
+    return result;
+};
 
 module.exports = { multiply };
--- questions/01_multiply/multiply.test.js
+++ questions/01_multiply/multiply.test.js
@@ -4,23 +4,27 @@ describe('multiply testing', () => {
   test('it should ensure it receives two numbers', () => {
     // test some values that should fail
 
-    expect(() => multiply(/*fill this in*/)).toThrow();
+    expect(() => multiply('1', 2)).toThrow();
 
-    expect(() => multiply(/*fill this in*/)).toThrow();
+    expect(() => multiply(1, '2')).toThrow();
   });
 
   test('it returns the product of two numbers', () => {
     // test the type of the returned value
-    expect(/*fill this in*/).toBe(/*fill this in*/);
+    expect(typeof(multiply(1,2))).toBe('number');
 
     // test that the returned value is correct
-    expect(/*fill this in*/).toBe(/*fill this in*/);
+    expect(multiply(1, 2)).toBe(2);
 
     // test some other values
-    expect(/* fill this in */).toBe(/* fill this in */);
+    expect(multiply(10, 2)).toBe(20);
   });
 
   test('it is not hardcoded (hint: use random numbers)', () => {
-    // test some random values
+    let num1 = Math.floor(Math.random() * 100);
+    console.log(num1);
+    let num2 = Math.floor(Math.random() * 100);
+    console.log(num2);
+    expect(multiply(num1, num2)).toBe(num1*num2);
   });
 });
