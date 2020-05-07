429d16b99945087d961ea8ad7e70734482d3686e
JudithBLinder <judithrooz@gmail.com>
2020-05-07 12:05:46 -0400

    trim solved

--- questions/08_trim/trim.js
+++ questions/08_trim/trim.js
@@ -1,7 +1,15 @@
 // trim takes an object and removes any keys that have undefined or null values
 
 const trim = obj => {
-  // YOUR CODE
+  const newObj = {};
+  for (let key in obj){
+    if (obj[key] == undefined || null){
+      continue
+    } else {
+      newObj[key] = obj[key];
+    };
+  };
+  return newObj;
 };
 
 module.exports = { trim };
