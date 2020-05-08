9e4ad755a60b3f71444239bb96d97756faef1a1a
JudithBLinder <judithrooz@gmail.com>
2020-05-08 16:57:44 -0400

    diceRoller solved

--- questions/09_diceRoller/DiceRoller.js
+++ questions/09_diceRoller/DiceRoller.js
@@ -3,7 +3,31 @@
 // history property which outputs all of the previous rolls
 
 class DiceRoller {
-  //YOUR CODE
+
+  constructor(sidesPerDie, numDice){
+    if (typeof(sidesPerDie) != 'number' || 
+        typeof(numDice) != 'number' ||
+        sidesPerDie <= 0 ||
+        numDice <= 0) {
+      throw 'wrong input!'
+    }
+    this.sidesPerDie = sidesPerDie;
+    this.numDice = numDice;
+    this.history = [];
+  }
+  
+  roll(){
+    let returnedArray = [];
+    for (let i=0; i<this.numDice; i++){
+      const max = this.sidesPerDie;
+      const min = 1;
+      // To get random number between two values
+      let randOut = Math.floor(Math.random() * (max - min) + min);
+      returnedArray.push(randOut);
+    };
+    this.history.push(returnedArray);
+    return returnedArray;
+  }
 }
 
 module.exports = { DiceRoller };
