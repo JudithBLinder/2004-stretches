f4badaff430a0ef31478134b1e97018639bffe38
JudithBLinder <judithrooz@gmail.com>
2020-05-09 09:09:04 -0400

    splatter solved

--- questions/10_splatter/splatter.js
+++ questions/10_splatter/splatter.js
@@ -1,7 +1,18 @@
 //write the splatter function
 
-const splatter = () => {
-  //write code here
+const splatter = (...arg) => {
+  console.log(arg);
+  const argList = Array.from(arg);
+  let val = 0;
+
+  console.log(argList)
+  argList.forEach(element => {
+    for (let key in element){
+      val += element[key];
+    }
+  });
+
+  return val;
 };
 
 module.exports = { splatter };
