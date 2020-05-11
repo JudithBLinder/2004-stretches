7028821eba82df0eadfe1c46bf52fb574f5c51d8
JudithBLinder <judithrooz@gmail.com>
2020-05-11 18:51:03 -0400

    fizzBuzz solved

--- questions/12_fizzBuzz/fizzBuzz.js
+++ questions/12_fizzBuzz/fizzBuzz.js
@@ -1,4 +1,20 @@
-const fizzBuzz = () => {
-  //YOUR CODE HERE
+const fizzBuzz = (num) => {
+  let returnedArray = [];
+  // console.log(Number.isInteger(num) , num);
+  if (!Number.isInteger(num) || num < 0) {
+    throw '';
+  }
+
+  for (let i=0; i<num; i++) {
+    if ((i + 1) % 3 ==0) {
+      returnedArray.push('Fizz');
+    } else if ((i + 1) % 5 ==0) {
+      returnedArray.push('Buzz');
+    } else {
+      returnedArray.push(i+1);
+    };
+  }
+  console.log(returnedArray);
+  return returnedArray
 };
 module.exports = { fizzBuzz };
