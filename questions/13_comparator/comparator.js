07ed82e752de954c08c8990f78dc5d348cd59518
JudithBLinder <judithrooz@gmail.com>
2020-05-12 13:55:38 -0400

    coparator solved

--- questions/13_comparator/comparator.js
+++ questions/13_comparator/comparator.js
@@ -1,5 +1,28 @@
 //write a func that takes two arg... selectKey and obj
 
-const comparator = () => {};
+const comparator = (keyIn, arrayIn) => {
+
+    if (!arrayIn[0][keyIn]) {
+        return 'incorrect key';
+    };
+
+    arrayIn.sort((a,b) => {
+        if (typeof(a[keyIn]) === 'number'){
+            return a[keyIn]-b[keyIn];
+        } else {
+            let aFirst = a[keyIn].split(' ')[1][0];
+            let bFirst = b[keyIn].split(' ')[1][0];
+            if (aFirst<bFirst) {
+                return -1;
+            };
+            if (aFirst>bFirst) {
+                return 1;
+            }
+            return 0;
+        } 
+    });
+
+    return arrayIn;
+};
 
 module.exports = { comparator };
