d85f1decc5d317c204d0e26881cce9918229a6a0
JudithBLinder <judithrooz@gmail.com>
2020-05-13 18:51:17 -0400

    movieposter solved

--- questions/14_movieposter/movieposter.js
+++ questions/14_movieposter/movieposter.js
@@ -7,8 +7,26 @@
 // * Play *
 // ********
 
-const movieposter = () => {
-  //write code here
+const movieposter = (arrayIn) => {
+  if (arrayIn.length === 0 || arrayIn === undefined) {
+    throw '';
+  };
+  let maxStrLength = 0;
+  arrayIn.forEach(el => {
+    if (el.length > maxStrLength) {
+      maxStrLength = el.length;
+    };
+  });
+
+  maxStrLength = maxStrLength + 4;
+  let strOut = ['*'.repeat(maxStrLength) + '\n'];
+
+  arrayIn.forEach(el => {
+    let tempStr = `* ${el} *\n`
+    strOut.push(tempStr)
+  })
+  console.log(strOut.join(''));
+  return strOut.join('');
 };
 
 module.exports = { movieposter };
