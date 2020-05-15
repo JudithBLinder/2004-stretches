9f446972ecd6d7c77fbbd6fa8ff6719c329a4675
JudithBLinder <judithrooz@gmail.com>
2020-05-15 15:13:25 -0400

    floydsTriangle solved

--- questions/16_floydsTriangle/floydsTriangle.js
+++ questions/16_floydsTriangle/floydsTriangle.js
@@ -6,6 +6,20 @@
 //  23
 //  456
 
-const floydsTriangle = n => {};
+const floydsTriangle = n => {
+    let floydsTriangleStr = '';
+    let startNum = 1;
+    debugger;
+    for (let i=0; i<n; i++) {
+        for (let j=0; j<i+1; j++) {
+            floydsTriangleStr += startNum;
+            startNum++
+        }
+        if (i<n-1) {
+            floydsTriangleStr += '\n';
+        }
+    }
+    return floydsTriangleStr;
+};
 
 module.exports = { floydsTriangle };
