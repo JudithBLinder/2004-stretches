5ed11dd5381a926573965f034395f72b47a2b856
JudithBLinder <judithrooz@gmail.com>
2020-05-16 09:11:00 -0400

    bookReview solved

--- questions/17_bookReview/bookReview.js
+++ questions/17_bookReview/bookReview.js
@@ -1,2 +1,26 @@
-class BookReview {}
+class BookReview {
+    constructor(title, author){
+        this.title = title;
+        this.author = author;
+        this.notes = [];
+        this.rating = 0;
+
+    }
+
+    addNotes(page, note){
+        if (this.notes[page]) {
+            this.notes[page] += ' ' + note;
+        } else {
+            this.notes[page] = note;
+        }
+        console.log(this.notes[15]);
+    }
+
+    addRating(rating){
+        if (rating > 5) {
+            throw '';
+        }
+        this.rating = rating;
+    }
+}
 module.exports = { BookReview };
