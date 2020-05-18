5800b16f5e9a09ae6df4d98dfb7ad6df70d7ffa2
JudithBLinder <judithrooz@gmail.com>
2020-05-18 18:57:23 -0400

    getrootvalue solved

--- questions/19_getrootvalue/getrootvalue.js
+++ questions/19_getrootvalue/getrootvalue.js
@@ -19,6 +19,23 @@
 //getrootvalue(object,40)=>'one'
 //returns -1 if value isn't found
 
-const getrootvalue = () => {};
+const getrootvalue = (objIn, val) => {
+    let root = '';
+
+    const findValue = (objIn) => {
+        if (Object.keys[objIn] === []) {
+            return '-1'
+        };
+        
+        for (let key in objIn) {
+            if (Array.isArray(objIn[key])) {
+                for (let i=0;)
+            }
+        }
+
+    }
+
+    return root;
+};
 
 module.exports = { getrootvalue };
