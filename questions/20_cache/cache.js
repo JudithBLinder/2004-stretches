61f5de223c08950548d816030da73e06e0a23df5
JudithBLinder <judithrooz@gmail.com>
2020-05-19 18:56:06 -0400

    cache solved

--- questions/20_cache/cache.js
+++ questions/20_cache/cache.js
@@ -1,7 +1,24 @@
 // see test specs
 
-function cache() {
-  // YOUR CODE
+function cache(funcIn) {
+  if (typeof funcIn != "function") {
+    throw new Error("Input must be a function.");
+  }
+
+  let prevInput = [];
+  let result = 0;
+
+  let funcOut = (...input) => {
+    if (input.join('') === prevInput.join('')) {
+      return result;
+    } else {
+      prevInput = input;
+      result = funcIn(...input);
+      return result;
+    }
+  };
+
+  return funcOut;
 }
 
 module.exports = { cache };
