01bda4d97687ef9644ea83133db0e44950f7caa6
JudithBLinder <judithrooz@gmail.com>
2020-05-21 22:02:49 -0400

    moveZeroes solved

--- questions/22_moveZeroes/moveZeroes.js
+++ questions/22_moveZeroes/moveZeroes.js
@@ -1,5 +1,21 @@
 const moveZeroes = (arr) => {
-  //YOUR CODE HERE
+  let counter = 0;
+
+  for (let i = 0; i < arr.length - counter; i++) {
+    console.log(arr, i, counter);
+    if (arr[i] === 0) {
+      for (let j = i; j < arr.length - 1; j++) {
+        arr[j] = arr[j + 1];
+      }
+      arr[arr.length - 1 - counter] = 0;
+      counter++;
+      if (arr[i] === 0 && i < arr.length - 1 - counter) {
+        i--;
+      }
+    }
+  }
+
+  return arr;
 };
 
 module.exports = { moveZeroes };
