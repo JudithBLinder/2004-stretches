77713f6efcaf4ba4e81f222aef2b5ea4353bdbfd
JudithBLinder <judithrooz@gmail.com>
2020-05-25 13:50:59 -0400

    extension solved

--- questions/23_extension/extension.js
+++ questions/23_extension/extension.js
@@ -1,5 +1,26 @@
 //write a function that takes a function, and makes it callable with two invocations
 
-const extensions = () => {};
+const extensions = (funcIn) => {
+  let funcToInvoke = funcIn;
+
+  if (typeof funcToInvoke !== 'function') {
+    throw new Error('Invalid Input');
+  }
+
+  return function firstInvocation(num1) {
+    console.log(num1);
+    if (typeof num1 !== 'number') {
+      throw new Error('Invalid Input');
+    }
+    // funcToInvoke(num1);
+    return function secondInvokation(num2) {
+      console.log(num2);
+      if (typeof num2 !== 'number') {
+        throw new Error('Invalid Input');
+      }
+      return funcToInvoke(num1, num2);
+    };
+  };
+};
 
 module.exports = { extensions };
