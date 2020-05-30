7b8cb47ee34cedb91e5e9868090e40673c8ef0e8
JudithBLinder <judithrooz@gmail.com>
2020-05-30 09:17:39 -0400

    btownbakery solved

--- questions/29_btownbakery/btownbakery.js
+++ questions/29_btownbakery/btownbakery.js
@@ -1,7 +1,24 @@
 // B-Town bakery over-bought the supplies to make cake and needs to know how many they can make all together.
 
-const btownbake = () => {
-  //enter code here
+const btownbake = (recipe, supplies) => {
+  if (typeof recipe !== 'object' || typeof supplies !== 'object') {
+    throw new Error('Invalid Input!');
+  }
+
+  let numOfCake = 0;
+  let ingArray = [];
+
+  for (let ing in recipe) {
+    if (supplies[ing] > recipe[ing]) {
+      ingArray.push(Math.floor(supplies[ing] / recipe[ing]));
+    } else {
+      ingArray.push(0);
+    }
+  }
+
+  numOfCake = Math.min(...ingArray);
+  console.log(numOfCake);
+  return numOfCake;
 };
 
 module.exports = { btownbake };
