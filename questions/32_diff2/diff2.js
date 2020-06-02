bbbcb9bc718516b38552850af918fd89b0fb1083
JudithBLinder <judithrooz@gmail.com>
2020-06-02 12:24:05 -0400

    diffTwo solved

--- questions/32_diff2/diff2.js
+++ questions/32_diff2/diff2.js
@@ -2,6 +2,26 @@
 // the returning array of pairs should be sorted in ascending order of values.
 //diffTwo([1, 2, 3, 4])=>[[1, 3], [2, 4]]
 
-const diffTwo = () => {};
+const diffTwo = (arrIn) => {
+  if (!Array.isArray(arrIn)) {
+    throw new Error('Invalid input!');
+  }
+
+  let arrOut = [];
+  let arrTemp = [];
+  arrIn.sort((a, b) => a - b);
+
+  for (let i = 0; i < arrIn.length; i++) {
+    arrTemp.push(arrIn[i]);
+    let nextInt = arrIn.find((el) => el - arrIn[i] === 2);
+    if (nextInt) {
+      arrTemp.push(nextInt);
+      arrOut.push(arrTemp);
+    }
+    arrTemp = [];
+  }
+
+  return arrOut;
+};
 
 module.exports = { diffTwo };
