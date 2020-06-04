52a62acaa506969339a26f7e31d11e94891f7bbe
JudithBLinder <judithrooz@gmail.com>
2020-06-04 15:09:25 -0400

    lockBox Solved

--- questions/05_lockBox/lockBox.js
+++ questions/05_lockBox/lockBox.js
@@ -1,5 +1,32 @@
-const lockBox = () => {
-  // write code here
+const lockBox = (initCode, initMsg) => {
+  if (typeof initCode != 'number' || typeof initMsg != 'string') {
+    throw new Error('Invalid Input!');
+  }
+
+  let currentCode = initCode;
+  let currentMsg = initMsg;
+
+  return {
+    modCode: function (code, newCode) {
+      if (code === currentCode) {
+        currentCode = newCode;
+      }
+      return currentCode;
+    },
+    modMessage: function (code, newMsg) {
+      if (code === currentCode) {
+        currentMsg = newMsg;
+      }
+      return currentMsg;
+    },
+    revealMessage: function (code) {
+      if (code === currentCode) {
+        return currentMsg;
+      } else {
+        throw new Error('Invalid Code!');
+      }
+    },
+  };
 };
 
 module.exports = { lockBox };
