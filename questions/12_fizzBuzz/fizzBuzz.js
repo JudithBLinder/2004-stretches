6ac3c627cd364378ab2db0c5251de78045f2bb3d
JudithBLinder <judithrooz@gmail.com>
2020-06-04 15:48:05 -0400

    FizzBuzz solved

--- questions/12_fizzBuzz/fizzBuzz.js
+++ questions/12_fizzBuzz/fizzBuzz.js
@@ -5,18 +5,19 @@ const fizzBuzz = (num) => {
     throw '';
   }
 
-  for (let i=0; i<num; i++) {
-    if ((i + 1) % 3 == 0) {
-      returnedArray.push('Fizz');
-    } else if ((i + 1) % 5 == 0) {
-      returnedArray.push('Buzz');
-    } else if ((i + 1) % 5 == 0 && (i + 1) % 3 == 0) {
-      returnedArray.push('FizzBuzz');
-    }else {
-      returnedArray.push(i+1);
-    };
+  for (let i = 0; i < num; i++) {
+    returnedArray[i] = i + 1;
+    if ((i + 1) % 3 === 0) {
+      returnedArray[i] = 'Fizz';
+    }
+    if ((i + 1) % 5 === 0) {
+      returnedArray[i] = 'Buzz';
+    }
+    if ((i + 1) % 15 === 0) {
+      returnedArray[i] = 'FizzBuzz';
+    }
   }
   console.log(returnedArray);
-  return returnedArray
+  return returnedArray;
 };
 module.exports = { fizzBuzz };
