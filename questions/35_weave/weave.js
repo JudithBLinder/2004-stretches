3652cfe5e5dcdddb03f45bab6a22fc9bd06e1f17
JudithBLinder <judithrooz@gmail.com>
2020-06-05 21:02:05 -0400

    weave solved

--- questions/35_weave/weave.js
+++ questions/35_weave/weave.js
@@ -2,8 +2,30 @@
 // if one array is longer than another place a null value in its allocated place
 // weave([1, 2, 3], [4, 5]) === [1, 4, 2, 5, 3, null]
 
-const weave = () => {
-  //enter code here
+const weave = (...arrays) => {
+  let maxSize = 0;
+  let arrOut = [];
+
+  arrays.forEach((arr) => {
+    if (arr.length > maxSize) {
+      maxSize = arr.length;
+    }
+    if (!Array.isArray(arr)) {
+      throw new Error('Invalid Input!');
+    }
+  });
+
+  for (let i = 0; i < maxSize; i++) {
+    for (let j = 0; j < arrays.length; j++) {
+      if (!arrays[j][i]) {
+        arrOut.push(null);
+      } else {
+        arrOut.push(arrays[j][i]);
+      }
+    }
+  }
+
+  return arrOut;
 };
 
 module.exports = { weave };
