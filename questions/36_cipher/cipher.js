d34a8001a7c29125d496c86ae6edac386e29d44f
JudithBLinder <judithrooz@gmail.com>
2020-06-07 21:18:10 -0400

    cipher solved

--- questions/36_cipher/cipher.js
+++ questions/36_cipher/cipher.js
@@ -1,5 +1,15 @@
-const encrypt = () => {
-  //YOUR CODE HERE
+const encrypt = (strIn) => {
+  let strArray = strIn.split(' ');
+  let newStrArray = [];
+  strArray.forEach((word) => {
+    newStrArray.push(
+      word.charCodeAt(0) +
+        word.charAt(word.length - 1) +
+        word.slice(2, word.length - 1) +
+        word.charAt(1)
+    );
+  });
+  return newStrArray.join(' ');
 };
 
 module.exports = { encrypt };
