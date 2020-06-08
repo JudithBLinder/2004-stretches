8a35050203a9ae0318fe78eb8e13e30e5765b67d
JudithBLinder <judithrooz@gmail.com>
2020-06-08 18:55:38 -0400

    cipher2 solved

--- questions/37_cipher2/cipher2.js
+++ questions/37_cipher2/cipher2.js
@@ -1,4 +1,14 @@
-const decypher = () => {
-  //YOUR CODE HERE
+const decypher = (strIn) => {
+  let wordArray = strIn.split('');
+  let newArray = [];
+  let charCode = '';
+
+  for (let i = 0; i < wordArray.length; i++) {
+    console.log(parseInt(0, 10).toString());
+    if (parseInt(wordArray[i], 10)) {
+      charCode += wordArray[i].toString();
+    }
+  }
+  console.log(charCode);
 };
 module.exports = { decypher };
