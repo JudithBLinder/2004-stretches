940c6dbf039e991de37e5a434b1e770084cc6f77
JudithBLinder <judithrooz@gmail.com>
2020-06-13 09:17:19 -0400

    part 2

--- questions/40_boxes/boxes.js
+++ questions/40_boxes/boxes.js
@@ -8,7 +8,16 @@ class Box {
   }
 
   pack(color, capacity) {
+    if (capacity === this.contents.length) {
+      throw new Error('Box Capacity Full');
+    }
     this.contents.push(new Box(color, capacity));
+  }
+
+  unpack() {
+    let result = [];
+    this.contents.forEach((box) => result.push(box.color));
+    return result;
   }
 }
 module.exports = { Box };
