9554fabd569be946c24ce2e6d1e4cb9325b09088
JudithBLinder <judithrooz@gmail.com>
2020-06-15 18:50:24 -0400

    iterator solved

--- questions/44_iterator/iterator.js
+++ questions/44_iterator/iterator.js
@@ -2,7 +2,20 @@
 // and method next() interates through it til complete
 
 class Iterator {
-  //write code here
+  constructor(arrIn) {
+    if (!Array.isArray(arrIn)) throw '';
+    this.arr = arrIn;
+    this.loggedIdx = 0;
+  }
+
+  next() {
+    this.loggedIdx++;
+    if (this.loggedIdx === this.arr.length + 1) {
+      return { done: true, value: undefined };
+    } else {
+      return { done: false, value: this.arr[this.loggedIdx - 1] };
+    }
+  }
 }
 
 module.exports = { Iterator };
