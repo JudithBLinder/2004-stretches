cfaa56897953a0619cbbb2cd916b957a667723d6
JudithBLinder <judithrooz@gmail.com>
2020-06-14 20:38:42 -0400

    part 2 not done

--- questions/40_boxes/boxes.js
+++ questions/40_boxes/boxes.js
@@ -1,3 +1,30 @@
 //YOUR CODE HERE
 
+class Box {
+  constructor(color, capacity) {
+    this.color = color;
+    this.capacity = capacity;
+    this.contents = [];
+  }
+
+  pack(color, capacity) {
+    if (this.capacity === this.contents.length) {
+      throw new Error('Box Capacity is Full');
+    }
+    this.contents.push(new Box(color, capacity));
+  }
+
+  unpack() {
+    let result = [];
+    const findColor = (box) => {
+      for (let i = 0; i < box.contents.length; i++) {
+        result.push(box.color);
+        findColor(box.contents[i]);
+      }
+    };
+    findColor(this);
+    console.log(result);
+    return result;
+  }
+}
 module.exports = { Box };
