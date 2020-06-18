b63f4fc6aff08d96cc085f6ec5e3c612fae990a3
JudithBLinder <judithrooz@gmail.com>
2020-06-18 16:27:25 -0400

    unsubscribe solved

--- questions/47_unsubscribe/unsubscribe.js
+++ questions/47_unsubscribe/unsubscribe.js
@@ -2,8 +2,13 @@
 
 class Messenger {
   constructor(channel) {
+    if (typeof channel !== 'object' && !channel.hasOwnProperty('name')) {
+      throw 'Invalid Input';
+    }
     this.channel = channel.name;
     this.listeners = {};
+    this.subscribe = this.subscribe.bind(this);
+    this.unsubscribe = this.unsubscribe.bind(this);
   }
   getChannel(type) {
     this.listeners[type] = this.listeners[type] || [];
@@ -11,6 +16,10 @@ class Messenger {
   }
   subscribe(type, fn) {
     this.getChannel(type).push(fn);
+    return () => this.unsubscribe(type);
+  }
+  unsubscribe(type) {
+    delete this.listeners[type];
   }
   publish(type, msg) {
     this.getChannel(type).forEach((listener) =>
