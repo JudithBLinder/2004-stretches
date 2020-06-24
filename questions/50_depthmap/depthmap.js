550f58878963851fddbf3be94dc242c55a4b5863
JudithBLinder <judithrooz@gmail.com>
2020-06-24 18:55:29 -0400

    depthmap

--- questions/50_depthmap/depthmap.js
+++ questions/50_depthmap/depthmap.js
@@ -2,8 +2,21 @@
 //Write a function to recursively "dig" through this object installing a depth key along the way
 //return the object with it's depth indicated
 
-const recordDepth = () => {
+const recordDepth = (objIn) => {
   //write code here
+  if (typeof objIn !== 'object') {
+    throw new Error('Invalid input!');
+  }
+
+  const insertDepth = (obj, depth) => {
+    obj.depth = depth;
+    let keys = Object.keys(obj);
+  
+    for (let i=0; i<keys.length; i++)
+    }
+  };
+
+  insertDepth(objIn, 0);
 };
 
 module.exports = { recordDepth };
