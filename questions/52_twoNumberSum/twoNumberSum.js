8e23a16349819f2a5c1ed0e9b176e127b9d75eb8
JudithBLinder <judithrooz@gmail.com>
2020-07-11 09:19:41 -0400

    not done

--- questions/52_twoNumberSum/twoNumberSum.js
+++ questions/52_twoNumberSum/twoNumberSum.js
@@ -5,8 +5,27 @@
 
 //***Your solution should NOT include nested loops of any type***
 
-const twoNumberSum = () => {
-  //write code here
+const twoNumberSum = (arrIn, target) => {
+  const findNum = (num1, arrIn, target) => {
+    if (num1 === arrIn[arrIn.length - 1]) {
+      return null;
+    } else {
+      return arrIn.filter((num2) => num1 + num2 === target);
+    }
+  };
+
+  for (let i = 0; i < arrIn.length; i++) {
+    let first = arrIn[i];
+    const res = findNum(first, arrIn, target);
+    console.log(res);
+    if (res === null) {
+      return null;
+    } else if (res === []) {
+      continue;
+    } else {
+      return [first, res[0]];
+    }
+  }
 };
 
 module.exports = { twoNumberSum };
