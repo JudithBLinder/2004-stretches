d1b291959cc21aa7669039adbaa0e1e2eb936c17
JudithBLinder <judithrooz@gmail.com>
2020-07-13 18:50:11 -0400

    done

--- questions/53_balancedArr/balancedArr.js
+++ questions/53_balancedArr/balancedArr.js
@@ -1,5 +1,16 @@
-const balanced = () => {
-  //YOUR CODE HERE
+const balanced = (arrIn) => {
+  let sum1 = 0;
+  let sum2 = arrIn.reduce((acc, curr) => acc + curr, 0) - arrIn[0];
+
+  for (let i = 0; i < arrIn.length; i++) {
+    console.log(sum1, sum2, i);
+    if (sum1 === sum2) {
+      return i;
+    }
+    sum1 += arrIn[i];
+    sum2 -= arrIn[i + 1];
+  }
+  return -1;
 };
 
 module.exports = balanced;
